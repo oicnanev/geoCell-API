1412ef247c7291d2b3964179fccc769e58914237
oicnanev <voicnanev@proton.me>
2023-12-23 19:52:01 +0000

    http-model-Problem

---
+++ geocell/src/main/kotlin/oicnanev/org/geocell/http/model/Problem.kt
@@ -0,0 +1,25 @@
+package oicnanev.org.geocell.http.model
+
+import org.springframework.http.ResponseEntity
+import java.net.URI
+
+class Problem(typeUri: URI) {
+    val type = typeUri.toASCIIString()
+
+    companion object {
+        const val MEDIA_TYPE = "application/problemjson"
+        const val GIT_URI = "https://github.com/oicnanev/geoCell-API/tree/main/geocell/"
+        const val DOCS_URI = "docs/problems/"
+
+        fun response(status: Int, problem: Problem) = ResponseEntity
+                .status(status)
+                .header("Content-Type", MEDIA_TYPE)
+                .body<Any>(problem)
+
+        val userAlreadyExists = Problem(URI("${GIT_URI}${DOCS_URI}user-already-exists"))
+        val insecurePassword = Problem(URI("${GIT_URI}${DOCS_URI}insecure-password"))
+        val userOrPasswordAreInvalid = Problem(URI("${GIT_URI}${DOCS_URI}user-or-password-are-invalid"))
+        val userNotFound = Problem(URI("${GIT_URI}${DOCS_URI}user-not-found"))
+        val invalidRequestContent = Problem(URI("${GIT_URI}${DOCS_URI}invalid-request-content"))
+    }
+}
