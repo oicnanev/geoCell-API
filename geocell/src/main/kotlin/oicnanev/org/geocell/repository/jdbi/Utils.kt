31f01cf65505c8cd72fac0e6cb7e8b8d48e75167
oicnanev <voicnanev@proton.me>
2023-12-24 12:39:20 +0000

    repository-jdbi-Utils-ConfigureWithAppRequirements

---
+++ geocell/src/main/kotlin/oicnanev/org/geocell/repository/jdbi/Utils.kt
@@ -0,0 +1,19 @@
+package main.kotlin.oicnanev.org.geocell.jdbi
+
+import main.kotlin.oicnanev.org.geocell.repository.jdbi.mappers.InstantMapper
+import main.kotlin.oicnanev.org.geocell.repository.jdbi.mappers.PasswordValidationInfoMapper
+import main.kotlin.oicnanev.org.geocell.repository.jdbi.mappers.TokenValidationInfoMapper
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.kotlin.KotlinPlugin
+import org.jdbi.v3.postgres.PostgresPlugin
+
+fun Jdbi.configureWithAppRequirements(): Jdbi {
+    installPlugin(KotlinPlugin())
+    installPlugin(PostgresPlugin())
+
+    registerColumnMapper(PasswordValidationInfoMapper())
+    registerColumnMapper(TokenValidationInfoMapper())
+    registerColumnMapper(InstantMapper())
+
+    return this
+}
